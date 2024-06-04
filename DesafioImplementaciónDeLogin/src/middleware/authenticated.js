438960aaa7599428ca67c3584cff270fccdb0f57
alexisjbarrientos <alexis.bj16@gmail.com>
2024-06-03 21:36:43 -0300

    correccion del login

--- DesafioImplementaciónDeLogin/src/middleware/authenticated.js
+++ DesafioImplementaciónDeLogin/src/middleware/authenticated.js
@@ -1,18 +1,3 @@
-// export const isAuthenticated = (req, res, next) => {
-//     if (req.session?.user) {
-//         return next();
-//     } else {
-//         res.redirect('/login');
-//     }
-// };
-
-// export const isNotAuthenticated = (req, res, next) => {
-//     if (!req.session?.user) {
-//         return next();
-//     } else {
-//         res.redirect('/profile');
-//     }
-// }
 
 export const isAuthenticated = (req, res, next) => {
     if (req.session.user) {
